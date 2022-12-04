47b813627204d397217acc8cf8b193140aeacbe2
大熊熊 <s86390474@yahoo.com>
2022-12-04 14:57:27 +0800

    mmm

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "<h1>hello spring  !</h1>";
+	}
+	
+	
+	
+}
